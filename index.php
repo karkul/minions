48bd117d68913c6a87b0cfed0802a4cd2ebb8799
Ramón Morales <karkul@users.noreply.github.com>
2017-03-09 17:06:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-$mode = 0; // 0 = simple mode; 1 = multiple
+$mode = 1; // 0 = simple mode; 1 = multiple
 
 $minions = array("minions","Dave","Jerry","Kevin","Phil","Stuart","Tim");
 
-echo "<h2>Minions show!</h2>";
+echo "<h2>BrainMeUp Minions show!</h2>";
 
 $id = 0;
 $image = $minions[0];
